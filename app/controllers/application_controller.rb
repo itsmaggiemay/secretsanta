6442cd69b9cdb67d7910828d069d091dcf7fcf5e
itsmaggiemay <mmh332@nyu.edu>
2014-12-18 02:32:22 -0500

    email confirmations work and new columns data is filled in the databases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 	
-	# def index
- #  	 render layout: 'application', text: '' 
-	# end
+	
+	def index
+  	 render layout: 'application', text: '' 
+	end
 
-		def index
-			render :index
-		end
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :email) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password) }
+end
+	
+
 		
 end
-
-
- 
