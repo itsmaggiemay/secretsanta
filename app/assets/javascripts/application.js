50d2daf5709c564310aa992f1a0fb9e41ad34101
itsmaggiemay <mmh332@nyu.edu>
2015-01-04 21:18:39 -0500

    fully working just not done with styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,18 +29,18 @@
 			}
 
 			return this;
-		}
+		};
 
 		/* Methods
 		*************************************************/
-		var randomizedParticipantsListIsUniqueAndValid = function (participants, poolOfPossibleParticipants) {
+	var randomizedParticipantsListIsUniqueAndValid = function (participants, poolOfPossibleParticipants) {
 			if (participants.length !== poolOfPossibleParticipants.length ) {
 				return false;
 			}
 			
 			for (var ii = 0, max = participants.length; ii < max; ii++) {
-				if ( participants[ii] === poolOfPossibleParticipants[ii] ) {
-					console.log( participants[ii], poolOfPossibleParticipants[ii] );
+				if ( participants[ii].name === poolOfPossibleParticipants[ii].name ) {
+					console.log( participants[ii].name, poolOfPossibleParticipants[ii].name );
 					return false;
 				}
 			}
@@ -76,11 +76,6 @@
 			return shuffledListOfParticipants;
 		};
 
-// for slider
-function outputUpdate(vol) {
-document.querySelector('#price').value = vol;
-}
-
 
 		$(function() {
 			$("div.row_containers").on("click", "a.add_name", function(event) {
@@ -92,43 +87,76 @@ document.querySelector('#price').value = vol;
 
 			$('#submitNames').on("click", function (event) {
 				var participants = [];
+				var party = [];
 
-				$('input.participant_name').each(function() {
-					var participant_name = $(this).val();
-					participants.push(participant_name);
-			});
+				$('input.participant_name').each(function(index) {
+					var participant_hash = {};
+					participant_hash.name = $(this).val();
+					participants.push(participant_hash);
+				});
+
+				$('input.participant_email').each(function(index){
+					var participant_email = $(this).val();
+					participants[index].email = participant_email;
+				});
+
+				$('input.organizer_name').each(function(index){
+					var party_hash = {};
+					party_hash.organizer_name = $(this).val();
+					party.push(party_hash);
+				});
+
+				$('input.party_name').each(function(index){
+					var party_name = $(this).val();
+					party[index].party_name = party_name;
+				});
+
+				$('input.party_date').each(function(index){
+					var party_date = $(this).val();
+					party[index].date_of_party = party_date;
+				});
+
+				$('input.budget').each(function(index){
+					var budget = $(this).val();
+					party[index].budget = budget;
+				});
+
+				$('textarea.message').each(function(index){
+					var message = $(this).val();
+					party[index].special_message = message;
+				});
+
+
 				var poolOfPossibleParticipants = randomizePoolOfPossibleParticpants(participants);
 				var partnersList = matchUpPeopleAndPartners(participants, poolOfPossibleParticipants);
+				var partyInformation = party;
 				var output = JSON.stringify(partnersList);
-				console.log( output );
 				
-
-				$.ajax({
+for (var ii = 0, max = participants.length; ii < max; ii++) {
+			$.ajax({
 					type: "POST",
 					url: "https://mandrillapp.com/api/1.0/messages/send.json",
 					data: {
-						'key': 'apikey',
+		
+						'key': ENV['MANDRILL_PASSWORD'],
 						'message': {
 						'from_email': 'mmh332@nyu.edu',
+						"from_name": "ELFST.R",
 						'to': [
 							{
-								'email': 'mmh332nyu@gmail.com',
-								'name': 'margaret',
-								'type': 'to'
-							},
-							{
-								'email': 'cesar.estelar@gmail.com',
-								'name': 'Cesar',
+								'email': partnersList[ii].person.email,
+								'name':  partnersList[1].person.name,
 								'type': 'to'
 							}
 							],
 						'autotext': 'true',
-						'subject': 'DOES THIS WORK?????',
-						'html': '<img src="http://3.bp.blogspot.com/-QBRuVpI-KyM/UNRFO0oZJBI/AAAAAAAAA5k/cbl8BRF01UM/s640/christomise.png"><br><h1>Happy Holidays! </h1>'
+						'subject': 'Secret Santa Exchange',
+						'html': "<img src='http://i.imgur.com/UHCtxpG.jpg'><br><br> Dear <strong>" +  partnersList[ii].person.name + "</strong><br> The reindeer have caught the dancing bug and will not be able to deliver presents this year. Will you please help me out?<br><br> My elves told me you will be going to a Christmas Party titled <strong>'" + partyInformation[0].party_name + "'</strong> on <strong>" + partyInformation[0].date_of_party	+ "</strong> . Could you please buy a present for <strong>"	+ partnersList[ii].partner.name + "</strong>? The maximum amount of money to spend is <strong> " + partyInformation[0].budget + "</strong>. <br><br> The party's organizer <strong>" + partyInformation[0].organizer_name + "</strong>, also left you a special note: <br>" + partyInformation[0].special_message + "<br><Br> I will be very grateful if you could be <strong>" + partnersList[ii].partner.name + "'s</strong> secret Santa this year. <br><br> Thanks!!!<br><Br> <img src='http://i.imgur.com/SEOnTiw.png'>"
 						}
 					}
 					}).done(function(response) {
 					console.log(response); // if you're into that sorta thing
 				});
+				}
 			});
 			});
